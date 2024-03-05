a2f5e58e720ae22c4823097677f5605e983ad266
Raju Sharma <113360219+razusharmaa@users.noreply.github.com>
2024-03-05 17:33:02 +0545

    Updates

--- src/components/Spinner.js
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-export default function Spinner() {
-  return (
-    <div className="spinner-border text-primary" role="status" style={{position: 'fixed', top: '92%', left: '50%'}}>
-    <span></span>
-  </div>
-  )
-}
